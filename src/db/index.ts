89668302a90d63a372c213ad6eb436e10566b318
Quang Vu <quangdv@uci.edu>
2026-07-14 17:50:42 -0700

    Switched to using Neon database

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,6 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+const sql = neon(process.env.DATABASE_URL!);
 
-export const db = drizzle(pool);
+export const db = drizzle(sql);
